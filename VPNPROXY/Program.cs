55ae667c915290a0f8373d2fd470963971cc40a1
dashM8 <tgamer553@gmail.com>
2017-09-11 22:12:53 +0300

    Add project files.

---
+++ VPNPROXY/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Titanium.Web.Proxy;
+
+
+namespace VPNPROXY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
